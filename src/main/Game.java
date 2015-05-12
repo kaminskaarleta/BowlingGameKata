4b7979ae4086d596c358b0df6781776a658c8984
Kamińska <180195@edu.p.lodz.pl>
2015-05-12 18:11:46 +0200

    Test 1 GREEN

---
+++ src/main/Game.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Game {
+
+	public void roll(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int score() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/test/BowlingGameTest.java
+++ src/test/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package test;
 import static org.junit.Assert.*;
+import main.Game;
 
 import org.junit.Test;
 
