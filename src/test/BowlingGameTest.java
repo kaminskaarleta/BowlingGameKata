0a381abca2a257f3e700d4ec199ddfd89bfff917
Kamińska <180195@edu.p.lodz.pl>
2015-05-12 19:42:13 +0200

    Test 4 REFACTOR

--- src/test/BowlingGameTest.java
+++ src/test/BowlingGameTest.java
@@ -1,53 +1,57 @@
 package test;
+
 import static org.junit.Assert.*;
 import main.Game;
 
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class BowlingGameTest {
 
 	private Game g;
-	
+
 	@Before
 	public void setUp() {
 		g = new Game();
 	}
-	
-	private void rollMany(int n, int pins){
+
+	private void rollMany(int n, int pins) {
 		for (int i = 0; i < n; i++)
 			g.roll(pins);
 	}
-	
-	private void rollSpare(){
+
+	private void rollSpare() {
 		g.roll(5);
 		g.roll(5);
 	}
-	
+
+	private void rollStrike() {
+		g.roll(10);
+	}
+
 	@Test
 	public void testGutterGame() {
 		rollMany(20, 0);
 		assertEquals(0, g.score());
 	}
-	
+
 	@Test
-	public void testAllOnes(){
+	public void testAllOnes() {
 		rollMany(20, 1);
 		assertEquals(20, g.score());
 	}
-	
+
 	@Test
 	public void testOneSpare() {
 		rollSpare();
 		g.roll(3);
-	    rollMany(17,0);
-	    assertEquals(16, g.score());
-	  }
-	
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+
 	@Test
 	public void testOneStrike() throws Exception {
-		g.roll(10); // strike
+		rollStrike();
 		g.roll(3);
 		g.roll(4);
 		rollMany(16, 0);
