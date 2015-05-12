bcfd1f68d27e9d90d9f4dae81828f42af084c0bd
Kamińska <180195@edu.p.lodz.pl>
2015-05-12 19:35:32 +0200

    Test 3 REFACTOR

--- src/main/Game.java
+++ src/main/Game.java
@@ -4,26 +4,29 @@ public class Game {
 
 	private int[] rolls = new int[21];
 	private int currentRoll = 0;
-	
+
 	public void roll(int pins) {
 		rolls[currentRoll++] = pins;
 	}
 
 	public int score() {
 		int score = 0;
-		int i = 0;
-				
-		for(int frame = 0 ; frame < 10 ; frame++){
-			if(rolls[i] + rolls[i + 1] == 10 ){ 
-				score += 10 + rolls[i + 2];
-				i += 2;
-			}else{
-				score += rolls[i] + rolls[i + 1];
-				i += 2;
+		int frameIndex = 0;
+
+		for (int frame = 0; frame < 10; frame++) {
+			if (isSpare(frameIndex)) {
+				score += 10 + rolls[frameIndex + 2];
+				frameIndex += 2;
+			} else {
+				score += rolls[frameIndex] + rolls[frameIndex + 1];
+				frameIndex += 2;
 			}
 		}
-				
 		return score;
+	}
+
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
 	}
 
 }
