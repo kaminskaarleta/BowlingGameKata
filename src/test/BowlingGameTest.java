7b8ecedd9f5a01f2f90af54a4e633cf4e54956df
Kamińska <180195@edu.p.lodz.pl>
2015-05-12 19:07:46 +0200

    Test 3 RED

--- src/test/BowlingGameTest.java
+++ src/test/BowlingGameTest.java
@@ -31,5 +31,14 @@ public class BowlingGameTest {
 		rollMany(20, 1);
 		assertEquals(20, g.score());
 	}
+	
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5);
+	    g.roll(3);
+	    rollMany(17,0);
+	    assertEquals(16, g.score());
+	  }
 
 }
